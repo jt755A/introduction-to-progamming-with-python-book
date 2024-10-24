4b8efbd8784c6f47e13bb91e6559a52940f12faf
jt755A <jot755A@gmail.com>
2024-10-24 10:11:54 -0700

    Adding Data Types Chapter

---
+++ data_types/exercise1.py
@@ -0,0 +1,9 @@
+'True' #String str
+False #Boolean bool
+(1, 2, 3) #Tuple, tuple
+1.5 #Float, float
+2 #Integer, int
+range(5) # Range, range
+{1, 2, 3} # Set, set
+None # NoneType, nonetype
+{'foo': 'bar'} #Dictionary, dict
