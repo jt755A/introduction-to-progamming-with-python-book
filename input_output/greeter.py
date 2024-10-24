76c95c0c7698d65fa987162f082be50904cf2d89
jt755A <jot755A@gmail.com>
2024-10-24 10:13:29 -0700

    Adding Input_Output Chapter

---
+++ input_output/greeter.py
@@ -0,0 +1,6 @@
+#NAME = input("What is your name? ")
+#print(f'Hello, {NAME}!')
+
+first_name = input("What is your first name?\n\n")
+last_name = input("What is your last name?\n\n")
+print(f'Hello, {first_name} {last_name}!')
---
+++ input_output/operate_numbers.py
@@ -0,0 +1,5 @@
+number1 = input('Enter the first number: ')
+number2 = input('Enter the second number: ')
+integer_divide = int(number1) // int(number2)
+
+print(f'The numbers {number1} and {number2} integer divide to {integer_divide}')
